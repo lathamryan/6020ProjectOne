a445149dfd11d040718ddbd3e2929aa6745ffa3a
lathamryan <42649830+lathamryan@users.noreply.github.com>
2018-09-17 06:12:51 -0600

    Upload of project one, both part 1 and 2

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class frmCalculateGrade : Form
+    {
+        public frmCalculateGrade()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            decimal numberGrade = Convert.ToDecimal(txtNumberGrade.Text);
+
+            string letterGrade = "";
+
+            if (numberGrade >= 88)
+            {
+                letterGrade = "A";
+            }
+            else if (numberGrade >= 80 && numberGrade < 88)
+            {
+                letterGrade = "B";
+            }
+            else if (numberGrade >= 68 && numberGrade < 80)
+            {
+                letterGrade = "C";
+            }
+            else if (numberGrade >= 60 && numberGrade < 68)
+            {
+                letterGrade = "D";
+            }
+            else
+            {
+                letterGrade = "F";
+            }
+
+            txtLetterGrade.Text = letterGrade;
+
+            txtNumberGrade.Focus();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
